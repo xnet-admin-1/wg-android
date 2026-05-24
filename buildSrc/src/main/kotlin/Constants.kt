8ee972b8c4eec07f4299b7d564492d1188f8c96e
xnet-admin-1 <xnet-admin-1@users.noreply.github.com>
2026-05-24 10:04:30 +0000

    bump versionCode to 40303

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
 	const val VERSION_NAME = "4.3.1"
-    const val VERSION_CODE = 40302
+    const val VERSION_CODE = 40303
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
